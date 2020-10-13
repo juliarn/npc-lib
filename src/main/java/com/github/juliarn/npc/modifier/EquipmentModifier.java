fbe9b64801af934b45d3728f326959982b761c30
Julian <42209193+juliarn@users.noreply.github.com>
2020-10-13 15:04:22 +0200

    Fix entity equipment packet for 1.16

--- src/main/java/com/github/juliarn/npc/modifier/EquipmentModifier.java
+++ src/main/java/com/github/juliarn/npc/modifier/EquipmentModifier.java
@@ -4,9 +4,12 @@ package com.github.juliarn.npc.modifier;
 import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.events.PacketContainer;
 import com.comphenix.protocol.wrappers.EnumWrappers;
+import com.comphenix.protocol.wrappers.Pair;
 import com.github.juliarn.npc.NPC;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Collections;
 
 public class EquipmentModifier extends NPCModifier {
 
@@ -17,8 +20,12 @@ public class EquipmentModifier extends NPCModifier {
     public EquipmentModifier queue(@NotNull EnumWrappers.ItemSlot itemSlot, @NotNull ItemStack equipment) {
         PacketContainer packetContainer = super.newContainer(PacketType.Play.Server.ENTITY_EQUIPMENT);
 
-        packetContainer.getItemSlots().write(MINECRAFT_VERSION < 9 ? 1 : 0, itemSlot);
-        packetContainer.getItemModifier().write(0, equipment);
+        if (MINECRAFT_VERSION < 16) {
+            packetContainer.getItemSlots().write(MINECRAFT_VERSION < 9 ? 1 : 0, itemSlot);
+            packetContainer.getItemModifier().write(0, equipment);
+        } else {
+            packetContainer.getSlotStackPairLists().write(0, Collections.singletonList(new Pair<>(itemSlot, equipment)));
+        }
 
         return this;
     }
@@ -26,8 +33,17 @@ public class EquipmentModifier extends NPCModifier {
     public EquipmentModifier queue(int itemSlot, @NotNull ItemStack equipment) {
         PacketContainer packetContainer = super.newContainer(PacketType.Play.Server.ENTITY_EQUIPMENT);
 
-        packetContainer.getIntegers().write(MINECRAFT_VERSION < 9 ? 1 : 0, itemSlot);
-        packetContainer.getItemModifier().write(0, equipment);
+        if (MINECRAFT_VERSION < 16) {
+            packetContainer.getIntegers().write(MINECRAFT_VERSION < 9 ? 1 : 0, itemSlot);
+            packetContainer.getItemModifier().write(0, equipment);
+        } else {
+            for (EnumWrappers.ItemSlot slot : EnumWrappers.ItemSlot.values()) {
+                if (slot.ordinal() == itemSlot) {
+                    packetContainer.getSlotStackPairLists().write(0, Collections.singletonList(new Pair<>(slot, equipment)));
+                    break;
+                }
+            }
+        }
 
         return this;
     }
