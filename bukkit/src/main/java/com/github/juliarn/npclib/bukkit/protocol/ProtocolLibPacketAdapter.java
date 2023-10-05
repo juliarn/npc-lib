075cb22c18f655c49b01ce4ace7fe8a5acfc7382
0utplay <aldin@sijamhodzic.de>
2023-10-05 21:55:21 +0200

    Start updating protocollib packets to 1.20.2

--- bukkit/src/main/java/com/github/juliarn/npclib/bukkit/protocol/ProtocolLibPacketAdapter.java
+++ bukkit/src/main/java/com/github/juliarn/npclib/bukkit/protocol/ProtocolLibPacketAdapter.java
@@ -32,6 +32,7 @@ import com.comphenix.protocol.events.PacketContainer;
 import com.comphenix.protocol.events.PacketEvent;
 import com.comphenix.protocol.utility.MinecraftReflection;
 import com.comphenix.protocol.utility.MinecraftVersion;
+import com.comphenix.protocol.wrappers.CustomPacketPayloadWrapper;
 import com.comphenix.protocol.wrappers.EnumWrappers;
 import com.comphenix.protocol.wrappers.MinecraftKey;
 import com.comphenix.protocol.wrappers.Pair;
@@ -79,6 +80,7 @@ import java.util.UUID;
 import java.util.function.BiFunction;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
@@ -237,12 +239,22 @@ final class ProtocolLibPacketAdapter implements PlatformPacketAdapter<World, Pla
   @Override
   public @NotNull OutboundPacket<World, Player, ItemStack, Plugin> createEntitySpawnPacket() {
     return (player, npc) -> {
-      // SpawnPlayer (https://wiki.vg/Protocol#Spawn_Player)
-      PacketContainer container = new PacketContainer(PacketType.Play.Server.NAMED_ENTITY_SPAWN);
+      PacketContainer container;
+      if (MinecraftVersion.CONFIG_PHASE_PROTOCOL_UPDATE.atOrAbove()) {
+        // SpawnEntity (https://wiki.vg/Protocol#Spawn_Entity)
+        container = new PacketContainer(PacketType.Play.Server.SPAWN_ENTITY);
+      } else {
+        // SpawnPlayer (https://wiki.vg/Protocol#Spawn_Player)
+        container = new PacketContainer(PacketType.Play.Server.NAMED_ENTITY_SPAWN);
+      }
 
       // base information
       container.getIntegers().write(0, npc.entityId());
       container.getUUIDs().write(0, npc.profile().uniqueId());
+
+      if (MinecraftVersion.CONFIG_PHASE_PROTOCOL_UPDATE.atOrAbove()) {
+        container.getEntityTypeModifier().write(0, EntityType.PLAYER);
+      }
 
       // position
       if (MinecraftVersion.COMBAT_UPDATE.atOrAbove()) {
@@ -461,22 +473,29 @@ final class ProtocolLibPacketAdapter implements PlatformPacketAdapter<World, Pla
       // CustomPayload (https://wiki.vg/Protocol#Custom_Payload)
       PacketContainer container = new PacketContainer(PacketType.Play.Server.CUSTOM_PAYLOAD);
 
-      // channel id
       if (MinecraftVersion.AQUATIC_UPDATE.atOrAbove()) {
         // mc 1.13: channel id is now in the format of a resource location
         String[] parts = channelId.split(":", 2);
         MinecraftKey key = parts.length == 1 ? new MinecraftKey(channelId) : new MinecraftKey(parts[0], parts[1]);
 
-        container.getMinecraftKeys().write(0, key);
+        if (MinecraftVersion.CONFIG_PHASE_PROTOCOL_UPDATE.atOrAbove()) {
+          // mc 1.20.2:
+          CustomPacketPayloadWrapper payloadWrapper = new CustomPacketPayloadWrapper(payload, key);
+          container.getCustomPacketPayloads().write(0, payloadWrapper);
+        } else {
+          container.getMinecraftKeys().write(0, key);
+        }
       } else {
         // mc 1.8: channel id is a string
         container.getStrings().write(0, channelId);
       }
 
-      // payload
-      ByteBuf buffer = Unpooled.copiedBuffer(payload);
-      Object wrappedSerializableBuffer = MinecraftReflection.getPacketDataSerializer(buffer);
-      container.getModifier().withType(ByteBuf.class).write(0, wrappedSerializableBuffer);
+      if (!MinecraftVersion.CONFIG_PHASE_PROTOCOL_UPDATE.atOrAbove()) {
+        // payload
+        ByteBuf buffer = Unpooled.copiedBuffer(payload);
+        Object wrappedSerializableBuffer = MinecraftReflection.getPacketDataSerializer(buffer);
+        container.getModifier().withType(ByteBuf.class).write(0, wrappedSerializableBuffer);
+      }
 
       // send the packet without notifying any bound packet listeners
       PROTOCOL_MANAGER.sendServerPacket(player, container, false);
