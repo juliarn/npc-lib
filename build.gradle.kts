1d25db1219db11c981667eaa4009228f0e00d37b
Pasqual Koschmieder <git@derklaro.dev>
2025-06-19 22:34:11 +0200

    release 3.0.0-beta13

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,7 @@ plugins {
 defaultTasks("clean", "build")
 
 allprojects {
-  version = "3.0.0-SNAPSHOT"
+  version = "3.0.0-beta13"
   group = "io.github.juliarn"
   description = "Abstract NPC-Library for Minecraft 1.8+ Servers"
 
