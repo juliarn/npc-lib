ae056ba9aca33a723433909b4a8573bec6d34c38
Pasqual Koschmieder <git@derklaro.dev>
2025-08-12 22:44:18 +0200

    chore: publish via central portal

--- build.gradle.kts
+++ build.gradle.kts
@@ -227,8 +227,8 @@ subprojects {
 nexusPublishing {
   repositories.run {
     sonatype {
-      nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
-      snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+      nexusUrl.set(uri("https://ossrh-staging-api.central.sonatype.com/service/local/"))
+      snapshotRepositoryUrl.set(uri("https://central.sonatype.com/repository/maven-snapshots/"))
 
       username.set(project.findProperty("ossrhUsername") as? String ?: "")
       password.set(project.findProperty("ossrhPassword") as? String ?: "")
