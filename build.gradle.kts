8776dcac81c397523a2a69f34b91aef3b7453cef
Pasqual Koschmieder <git@derklaro.dev>
2026-07-30 23:53:16 +0200

    release 3.0.0-beta.17

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,7 @@ plugins {
 defaultTasks("clean", "build")
 
 allprojects {
-  version = "3.0.0-SNAPSHOT"
+  version = "3.0.0-beta.17"
   group = "io.github.juliarn"
   description = "NPC-Library for Minecraft 1.8+ Servers"
 
